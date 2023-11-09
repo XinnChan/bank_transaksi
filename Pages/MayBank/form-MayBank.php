82809f61faba1b6937ed3061afb55abd7011f5fd
XinnChan <98129742+XinnChan@users.noreply.github.com>
2023-11-09 18:08:26 +0700

    Add files via upload

---
+++ Pages/MayBank/form-MayBank.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Form-MAYBANK</title>
+    <link rel="stylesheet" href="../../style/Responsive.css" />
+    <link rel="stylesheet" href="../../style/style.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  </head>
+  <body class="bg-dark">
+    <div class="container-hp border border-dark shadow-lg text-left">
+      <div class="container-lg">
+      <div class="logo-bank">
+        <img src="../../image/MayBank.png" class="w-100 position-relative" style="margin-top: -60px;" alt="Logo-BankIslam">
+      </div>
+      <div class="text d-flex align-items-center">
+<div  style="background-color: #1664BC; width:4em; height:0.2em; border-radius: 10px;"></div>&nbsp;&nbsp;&nbsp;<h5 style="color: #1664BC; font-family: 'Poppins',sans-serif ; font-weight: bold;" class="text-1xl">BANK MAYBANK </h5>
+      </div>
+    <!-- form input data ke json  -->
+      <form action="hasil.php" method="post" class="container-lg " >
+        <label for="inputNama" class="form-label text-left fw-bold text-3xl" style="font-family: 'Poppins',sans-serif">Nama Penerima</label>
+        <input type="text" id="nama" name="nama" class="form-control" placeholder="Nama Penerima"/>
+        <label for="inputNumber" class="form-label text-left fw-bold text-3xl" style="font-family: 'Poppins',sans-serif">Number Account</label>
+        <input type="text" id="number" name="number_account" class="form-control" placeholder="Account Number"/>
+        <label for="PilihBank" class="text-left text-3xl fw-bold" style="font-family:'Poppins',sans-serif">Pilih Bank</label>
+        <!-- input pilihan bank -->
+        <div class="button-dropdown mt-2 m-1">
+        <select name="pilih" class="btn btn-primary w-100 active"  id="cars">
+          <option >Pilih Bank</option>
+          <option type="text"  value="AmbangGroup" >AmbangGroup</option>
+          <option type="text"  value="BankRakyat">BankRakyat</option>
+          <option type="text"  value="BankIslam">BankIslam</option>
+          <option type="text"  value="CIMB">CIMB</option>
+          <option type="text"  value="HongLeong">HongLeong</option>
+          <option type="text"  value="MayBank">MayBank</option>
+          <option type="text"  value="Brimo">Brimo</option>
+        </select>
+<!-- akhir input -->
+        </div>
+        <label for="references" class="text-3xl fw-bold " style="font-family:'Poppins',sans-serif">References</label>
+        <input type="text" class="form-control" name="reference" placeholder="Reference Transaction" />
+        <label for="description" class="text-3xl fw-bold " style="font-family: 'Poppins',sans-serif;" >Description</label>
+  <textarea class="form-control rounded-0" id="exampleFormControlTextarea2" rows="3" name="description" placeholder="Enter Description" ></textarea>
+  <label for="jumlahrm" class="text-3xl fw-bold" style="font-family: 'Poppins',sans-serif;">Jumlah RM</label>
+  <input type="text" class="form-control" name="jumlah" placeholder="Total Transfer Min 1000" />
+  <button type="submit" name="submit" value="submit" class="btn btn-primary w-50 mt-2">Submit</button>
+      </form>
+    <div class="gif">
+      <img src="../../image/data.gif" alt="data-gif" class="w-100" >
+    </div>
+    </div>
+</div>
+</body>
+</html>
---
+++ Pages/MayBank/hasil.php
@@ -0,0 +1,132 @@
+<!-- get data dari form utama -->
+
+<?php 
+$nama = $_POST['nama'];
+$number_account = $_POST['number_account'];
+$pilih = $_POST['pilih'];
+$reference = $_POST['reference'];
+$description = $_POST['description'];
+$jumlah = $_POST['jumlah'];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Hasil</title>
+    <link rel="stylesheet" href="../../style/Responsive.css" />
+    <link rel="stylesheet" href="../../style/style.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  </head>
+  <body class="bg-dark">
+  <!-- mobile web -->
+  <div class="container-hp  border border-dark shadow-lg text-center" style="height: 900px;">
+    <div class="container-lg">
+        <div class="block border border-dark mt-4  ">
+           <div class=" w-full left-0  d-flex  align-items-start" style="background-color: #FFD000;">
+           <img src="maybank.png" class="ms-4" style="width: 20%; margin: 10px; " alt="">
+           </div>
+
+           <div class="w-full d-flex justify-content-between p-2 align-items-center relative " style="background-color: #EDEDED;">
+            <h6 class="fw-bold m-2 ms-4" style="font-family:'Poppins',sans-serif; font-weight:normal; font-size: 10px;">DuitNow Transfer</h6>
+            <div class="me-4 m-2 text-center "
+            style="
+            background-color: #00E100;
+            width: 80px;
+            height: 20px;
+            border-radius: 10px;
+            "
+            >
+              <div class="text-white position-relative" style="top: 3px; font-weight:lighter; font-family: 'Roboto',sans-serif;font-size: 10px;">Successful</div>
+            </div>
+            </div>
+
+            <div class=" w-full text-start  p-1 border-dark border-bottom " style="background-color: #FFFFFF; ">
+            <div class="m-2 ms-4">
+              <div class="row align-items-center">
+                <div class="col">
+                  <div class="">Reference ID</div>
+                  <div class="fw-bold" style="font-family: 'Poppins',sans-serif; font-size: 10px;"><?php echo"$reference" ?></div>
+                  <br />
+                  <div>Beneficiary name</div>
+                  <div class="fw-bold"  style="font-family: 'Poppins',sans-serif; font-size: 10px; "><?php echo"$nama" ?></div>
+                </div>
+              
+
+              <div class="col">
+              <div class="m-2 mm-2 ms-4 me-5"style="color: #939393; position:relative;;font-family:'Poppins',sans-serif; top: -25px  ;font-size: 10px;"><?php 
+              date_default_timezone_set('Asia/Jakarta');
+                $waktu_indonesia = date(' H:i:s A');
+               echo strftime('%d %b %Y, %H:%M') . $waktu_indonesia; ?>
+               </div>
+              </div>
+</div>
+              
+            </div>
+
+            </div>
+        
+
+        <div class=" w-full text-start p-1 border-dark border-bottom " style="background-color: #FFFFFF; ">
+            <div class="m-2 mm-2 ms-4">
+               <div class="row align-items-center">
+                <div class="col">
+                  <div style="font-size: 10px;">Beneficiary account Number</div>
+                  <div class="fw-bold"  style="font-family: 'Poppins',sans-serif ;font-size: 10px;"><?php echo"$jumlah" ?></div>
+
+                  <br />
+
+                  <div style="font-size: 10px;">Receiving Bank</div>
+                  <div class="fw-bold" style="font-family: 'Poppins',sans-serif;font-size: 10px;"><?php echo"$pilih" ?></div>
+                </div>
+               
+            
+            </div>
+            </div>
+        </div>
+
+        <div class=" w-full text-start p-1 border-dark border-bottom " style="background-color: #FFFFFF; font-size: 10px;">
+            <div class="m-2 ms-4">
+               <div class="row align-items-center">
+                <div class="col">
+                  <div>Receiving reference</div>
+                  <div class="fw-bold"  style="font-family: 'Poppins',sans-serif"><?php echo"Suhaida" ?></div>
+            </div>
+            </div>
+        </div>
+        </div>  
+
+        <div class=" w-full text-start p-1 border-dark border-bottom " style="background-color: #FFFFFF; font-size: 10px;">
+            <div class="m-2 ms-4">
+               <div class="row align-items-center">
+                <div class="col">
+                  <div>Amount</div>
+                  <div class="fw-bold"  style="font-family: 'Poppins',sans-serif"><?php echo"RM &nbsp;" . number_format($jumlah,2) ?></div>
+            </div>
+            </div>
+        </div>
+        </div>  
+
+        <div class=" w-full text-start p-1 border-dark border-bottom " style="background-color: #FFFFFF; font-size: 10px;">
+            <div class="m-2 ms-4">
+               <div class="row align-items-center">
+                <div class="col " style="color:#737373">
+                  <div>Malayan Banking Berhad (Co. Reg. : 196001000142)</div>
+                  <div>Malayan Banking Berhad (Co. Reg. : 200701029411)</div>
+            </div>
+            </div>
+        </div>
+        </div>  
+
+       
+
+
+    </div></br>
+
+  </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  </body>
+</html>
+
